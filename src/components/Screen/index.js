734353a056a83f443951121dd3918e105bc1f25f
Guseinov Muslim <gusejnov1999@list.ru>
2018-07-14 01:34:26 +0300

    @create(Screen)

---
+++ src/components/Screen/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Content, Container } from 'native-base';
+import FooterNav from '../FooterNav'
+import Header from '../Header'
+
+class Screen extends Component {
+  static propTypes = {};
+
+  componentDidMount() {
+
+  }
+
+  handleBack = () => {
+    console.log('back');
+  };
+
+  handleOpenMenu = () => {
+    console.log('back');
+  };
+
+  render() {
+    const { title, children } = this.props;
+
+    return (
+      <Container>
+        <Header
+          title={title}
+          handleBack={this.handleBack}
+          handleOpenMenu={this.handleOpenMenu}
+        />
+        { children }
+        <FooterNav />
+      </Container>
+    );
+  }
+}
+
+export default Screen;
