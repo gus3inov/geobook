2d1b9563b6a7c37f7f7440888ac985138daa2b8c
Guseinov Muslim <gusejnov1999@list.ru>
2018-07-14 01:34:04 +0300

    @create(Header)

---
+++ src/components/Header/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Container,
+  Header as NativeHeader,
+  Left,
+  Body,
+  Right,
+  Button,
+  Icon,
+  Title,
+} from 'native-base';
+import styles from './style';
+
+const Header = (props) => {
+  const {
+    title,
+    handleBack,
+    handleOpenMenu,
+  } = props;
+
+  return (
+    <Container>
+      <NativeHeader>
+        <Left>
+          <Button onPress={handleBack} transparent>
+            <Icon name="arrow-back" />
+          </Button>
+        </Left>
+        <Body>
+        <Title>
+          { title }
+        </Title>
+        </Body>
+        <Right>
+          <Button onPress={handleOpenMenu} transparent>
+            <Icon name="menu" />
+          </Button>
+        </Right>
+      </NativeHeader>
+    </Container>
+  );
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  handleBack: PropTypes.func.isRequired,
+  handleOpenMenu: PropTypes.func.isRequired,
+};
+
+export default Header;
